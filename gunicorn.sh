c97992fa46167c1e5e78ecd0f234e7a8793518ce
Talat Parwez <talatparwez@Talats-MacBook-Pro.local>
2024-08-18 00:48:46 +0530

    fixes

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,28 +2,8 @@
 
 cd /app 
 . /venv/bin/activate
-echo "Now listing folders"
-ls -lah
 
 python3 bookmanagement/manage.py makemigrations
 python3 bookmanagement/manage.py migrate
 
-echo "Migrations are applied"
-
 exec gunicorn --bind 0.0.0.0:8000 bookmanagement.bookmanagement.wsgi
-
-# # cd /var/lib/jenkins/workspace/Django-jenkins/
-# cp -rf gunicorn.socket /etc/systemd/system/
-# cp -rf gunicorn.service /etc/systemd/system/
-
-# echo "$USER"
-# echo "$PWD"
-
-# systemctl daemon-reload
-# systemctl start gunicorn
-# systemctl enable gunicorn
-
-# echo "GUNICORN has been started"
-
-# sudo systemctl status gunicorn
-# sudo systemctl restart gunicorn
