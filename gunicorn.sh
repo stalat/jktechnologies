25a0b08dd16d97f0535164b68b7fa0b3196fb27e
Talat Parwez <talatparwez@Talats-MacBook-Pro.local>
2024-08-18 01:47:14 +0530

    fixes

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "Listing all the files"
-ls -lah
-cd /app 
 . /venv/bin/activate
+cd /app 
 
-python3 /app/manage.py makemigrations
-python3 /app/manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 exec gunicorn --bind 0.0.0.0:8000 bookmanagement.wsgi
