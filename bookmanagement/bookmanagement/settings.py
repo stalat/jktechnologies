0b321477f4a7444994f6697ec3477defa0ec9097
Talat Parwez <talatparwez@Talats-MacBook-Pro.local>
2024-08-18 01:14:36 +0530

    fix

--- bookmanagement/bookmanagement/settings.py
+++ bookmanagement/bookmanagement/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bookmanagement.urls'
+ROOT_URLCONF = 'bookmanagement.bookmanagement.urls'
 
 TEMPLATES = [
     {
