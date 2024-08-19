7467c64211315d976aa79be752913a6ff1e7a97b
Talat Parwez <talatparwez@Talats-MacBook-Pro.local>
2024-08-20 00:32:54 +0530

    file change

--- gunicorn.sh
+++ gunicorn.sh
@@ -9,4 +9,5 @@ cd /app
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-exec gunicorn --bind 0.0.0.0:8000 jktechnologies.wsgi:application
+
+gunicorn jktechnologies.wsgi:application --bind 0.0.0.0:8000
